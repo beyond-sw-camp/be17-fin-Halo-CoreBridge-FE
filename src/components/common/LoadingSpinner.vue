caed50400bd842d5e80ac8c31cee840f0d84cb6c
sangwoo <lesw1216@gmail.com>
2025-10-21 01:43:28 +0900

    feat: Loading Spinner 구현

---
+++ src/components/common/LoadingSpinner.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { LoaderCircle } from 'lucide-vue-next';
+import { useLoadingStore } from '@/store/useLoadingStore';
+
+const loadingStore = useLoadingStore()
+
+</script>
+
+<template>
+    <div v-if="loadingStore.isLoading" class="fixed inset-0 flex justify-center items-center z-50 bg-white/70">
+        <LoaderCircle class="animate-spin text-slate-600" size="45" />
+    </div>
+</template>
+
+<style scoped></style>
