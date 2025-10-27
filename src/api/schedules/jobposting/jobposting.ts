4bb4056e3b566e5b7b6474718b5dde4a91c688db
염준선 <wnstjs1031@naver.com>
2025-10-27 18:09:07 +0900

    feat: 채용공고 기본정보(헤더) 요청 api 생성

--- src/api/schedules/jobposting/jobposting.ts
+++ src/api/schedules/jobposting/jobposting.ts
@@ -52,6 +52,30 @@ export const getJobs = async (): Promise<ApiResponse> => {
   return data
 }
 
+export const getHeaderById = async (id: number): Promise<ApiResponse> => {
+  let data: ApiResponse = {
+    success: false,
+    code: 0,
+    message: '',
+    results: undefined,
+  }
+
+  const url = `/api/job-postings/header/${id}`
+
+  await api
+  .get(url)
+  .then((res) => {
+    console.log(res)
+    data = res.data
+  })
+  .catch((error) => {
+    console.log(error)
+    data = error.response.data as ApiResponse
+  })
+
+  return data
+}
+
 export const getJobById = async (id: number): Promise<ApiResponse> => {
   let data: ApiResponse = {
     success: false,
