d6431add9da21606c195a36c43c4038887a90c5e
sangwoo <lesw1216@gmail.com>
2025-10-27 15:03:33 +0900

    refactor: RecruitProcess.ts의 RecruitProcess interface 필드 수정

--- src/types/jobPosting/RecruitProcess.ts
+++ src/types/jobPosting/RecruitProcess.ts
@@ -1,7 +1,9 @@
+import type { ColorCode } from '@/types/common/ColorCode.ts'
+
 export interface RecruitProcess {
   id: number
   name: string
-  colorCode: string
+  colorCode: ColorCode
   orderIdx: number
 }
 
